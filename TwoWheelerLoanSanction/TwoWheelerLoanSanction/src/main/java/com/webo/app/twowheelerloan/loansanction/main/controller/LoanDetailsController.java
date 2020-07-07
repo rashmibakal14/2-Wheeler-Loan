efc959327c14dc0711ab3dce7279208339cf6d8a
rashmi <rbakal9@gmial .com>
2020-07-07 20:01:01 +0530

    dek
    update

--- TwoWheelerLoanSanction/TwoWheelerLoanSanction/src/main/java/com/webo/app/twowheelerloan/loansanction/main/controller/LoanDetailsController.java
+++ TwoWheelerLoanSanction/TwoWheelerLoanSanction/src/main/java/com/webo/app/twowheelerloan/loansanction/main/controller/LoanDetailsController.java
@@ -1,6 +1,6 @@
 package com.webo.app.twowheelerloan.loansanction.main.controller;
 
-
+     
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
